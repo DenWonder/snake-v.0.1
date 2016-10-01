32ee9f0556ddd9b7174902f0d56b681e528833e5
DenWonder <denis.chudnov.97@gmail.com>
2016-10-01 20:06:58 +0700

    Hello,world

---
+++ snake v.0.1/snake v.0.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake_v._0._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,world!");
+            Console.ReadLine();
+        }
+    }
+}
